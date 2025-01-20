64dfde3938433038561b43aff00ba8d44190a87b
peter kneale <peterkneale@gmail.com>
2025-01-20 16:24:12 +1100

    update comments

--- meters/tests/meter_calculator.test.js
+++ meters/tests/meter_calculator.test.js
@@ -2,17 +2,17 @@ const { MeterBuilder } = require("./builders/meter_builder");
 const { MeterCollectionBuilder } = require("./builders/meter_collection_builder");
 const { MeterCalculator } = require("../src/meter_calculator");
 describe("Meter Calculator Tests", () => {
-  it("should add both active loads and ignore the inactive load", () => {
+  it("should total the loads mamtching the status", () => {
     const collection = new MeterCollectionBuilder()
       .withMeter(new MeterBuilder().withAveragedDailyLoad(1).withStatusActive().build())
       .withMeter(new MeterBuilder().withAveragedDailyLoad(5).withStatusInActive().build())
       .withMeter(new MeterBuilder().withAveragedDailyLoad(2).withStatusActive().build())
       .build();
 
-    var result = MeterCalculator.calculateTotalAverageDailyByStatus(collection, "A");
-    expect(result).toBe(3);
+    var activeLoad = MeterCalculator.calculateTotalAverageDailyByStatus(collection, "A");
+    expect(activeLoad).toBe(3);
 
-    var result2 = MeterCalculator.calculateTotalAverageDailyByStatus(collection, "I");
-    expect(result2).toBe(5);
+    var inactiveLoad = MeterCalculator.calculateTotalAverageDailyByStatus(collection, "I");
+    expect(inactiveLoad).toBe(5);
   });
 });
